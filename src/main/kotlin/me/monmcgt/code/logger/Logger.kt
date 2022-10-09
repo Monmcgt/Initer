58d25f23a8dd7c738587dd6ffbfb358cac44a192
Monmcgt <raninmon2015@gmail.com>
2022-10-10 03:39:33 +0700

    add pid in Logger

--- src/main/kotlin/me/monmcgt/code/logger/Logger.kt
+++ src/main/kotlin/me/monmcgt/code/logger/Logger.kt
@@ -5,7 +5,7 @@ import me.monmcgt.code.util.formattedDate
 object Logger {
     fun info(message: String) {
         ProcessBuilder("/usr/bin/tee", "-a", "/tmp/.monmcgt/initer/latest.log").start().outputStream.bufferedWriter().use {
-            it.write("[${formattedDate()}] [INFO] $message\n")
+            it.write("[${formattedDate()}] (${ProcessHandle.current().pid()}) [INFO] $message\n")
         }
     }
 }
