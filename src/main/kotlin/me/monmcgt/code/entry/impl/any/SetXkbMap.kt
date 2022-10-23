06f3cc0d7ad1bb6b1a88abed3a5ceb7262c0ae58
Monmcgt <raninmon2015@gmail.com>
2022-10-23 14:14:22 +0700

    fix setxkbmap

--- src/main/kotlin/me/monmcgt/code/entry/impl/any/SetXkbMap.kt
+++ src/main/kotlin/me/monmcgt/code/entry/impl/any/SetXkbMap.kt
@@ -4,9 +4,10 @@ import me.monmcgt.code.entry.Entry
 import me.monmcgt.code.entry.Register
 import me.monmcgt.code.util.runCommand
 
-@Register(onlyOnce = true)
+@Register
 class SetXkbMap : Entry() {
     override fun execute() {
+        Thread.sleep(5000)
         runCommand("/usr/bin/setxkbmap", "-option", "ctrl:nocaps")
     }
 }
