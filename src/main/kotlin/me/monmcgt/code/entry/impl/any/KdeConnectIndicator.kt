6e67c168b5d965601b36f61896e16477c53c724c
Monmcgt <raninmon2015@gmail.com>
2022-11-09 20:31:03 +0700

    KdeConnectIndicator add sleep

--- src/main/kotlin/me/monmcgt/code/entry/impl/any/KdeConnectIndicator.kt
+++ src/main/kotlin/me/monmcgt/code/entry/impl/any/KdeConnectIndicator.kt
@@ -4,11 +4,13 @@ import me.monmcgt.code.entry.Entry
 import me.monmcgt.code.entry.Register
 import me.monmcgt.code.util.killProcess
 import me.monmcgt.code.util.runCommand
+import me.monmcgt.code.util.sleep
 
 @Register
 class KdeConnectIndicator : Entry() {
     override fun execute() {
         killProcess("kdeconnect-indicator", true)
+        sleep(2500)
         runCommand("/usr/bin/kdeconnect-indicator")
     }
 }
