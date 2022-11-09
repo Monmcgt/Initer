f87fe05dbe8eded51d5b83140382097bc8f7346a
Monmcgt <raninmon2015@gmail.com>
2022-11-09 20:19:14 +0700

    kill CopyQ when run

--- src/main/kotlin/me/monmcgt/code/entry/impl/any/CopyQ.kt
+++ src/main/kotlin/me/monmcgt/code/entry/impl/any/CopyQ.kt
@@ -3,10 +3,13 @@ package me.monmcgt.code.entry.impl.any
 import me.monmcgt.code.entry.Entry
 import me.monmcgt.code.entry.Register
 import me.monmcgt.code.util.runCommand
+import me.monmcgt.code.util.sleep
 
 @Register
 class CopyQ : Entry() {
     override fun execute() {
+        runCommand("/usr/bin/copyq", "exit")?.waitFor()
+        sleep(2000)
         runCommand("/usr/bin/copyq")
     }
 }
