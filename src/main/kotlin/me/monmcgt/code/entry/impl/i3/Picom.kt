c83aee56e4ec39601ff3872de4cf83587807c27e
Monmcgt <raninmon2015@gmail.com>
2022-11-13 15:42:13 +0700

    update Picom.kt

--- src/main/kotlin/me/monmcgt/code/entry/impl/i3/Picom.kt
+++ src/main/kotlin/me/monmcgt/code/entry/impl/i3/Picom.kt
@@ -5,11 +5,22 @@ import me.monmcgt.code.entry.Entry
 import me.monmcgt.code.entry.Register
 import me.monmcgt.code.util.killProcess
 import me.monmcgt.code.util.runCommand
+import java.io.File
 
 @Register(DesktopSessions.I3)
 class Picom : Entry() {
     override fun execute() {
         killProcess("picom", true)
-        runCommand("/usr/bin/picom", "-f")
+        runCommand("/usr/bin/picom", readRunArgs())
+    }
+
+    private fun readRunArgs(): Array<String> {
+        val configDir = System.getenv("XDG_CONFIG_HOME") ?: "${System.getProperty("user.home")}/.config"
+        val argsFile = File("$configDir/picom/args.txt")
+        return if (argsFile.exists()) {
+            argsFile.readLines().map { line -> line.split("\\s+".toRegex()) }.flatten().filter { it.isNotEmpty() }.toTypedArray()
+        } else {
+            emptyArray()
+        }
     }
 }
